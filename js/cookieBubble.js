93f02e6e8bdff83be08e07a1b561ea743e77cd8e
João Pereira <joaopereirawd@gmail.com>
2018-06-08 10:18:49 +0100

    UPD: improvements

--- js/cookieBubble.js
+++ js/cookieBubble.js
@@ -50,60 +50,17 @@ jQuery.fn.cookieBubble = function( options ) {
 				    }
 				},
 				applyCookie: function(godown) {
-					cookieApp.setCookie(defaults.cookieBubbleName, defaults.maxAge); 
+					cookieApp.setCookie(defaults.cookieBubbleName, true, defaults.maxAge); 
 					$('.cookieBubble').hide();
 				}
 
     		}
 
 
+			
     		$('.accept-btn').click(function(event) {
 				cookieApp.applyCookie();
 			});
-
-
- 			/*function setCookie(cname,cvalue,exdays) {
-			    var d = new Date();
-			    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-			    var expires = "expires=" + d.toGMTString();
-			    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-			}
-
-			function getCookie(cname) {
-			    var name = cname + "=";
-			    var decodedCookie = decodeURIComponent(document.cookie);
-			    var ca = decodedCookie.split(';');
-			    for(var i = 0; i < ca.length; i++) {
-			        var c = ca[i];
-			        while (c.charAt(0) == ' ') {
-			            c = c.substring(1);
-			        }
-			        if (c.indexOf(name) == 0) {
-			            return c.substring(name.length, c.length);
-			        }
-			    }
-			    return "";
-			}
-
-			function checkCookie() {	
-			    var get_cookie = getCookie("cookieBubble");
-
-			    if (get_cookie != "" && get_cookie != null) {
-			        //console.log(get_cookie);
-			    } else {
-			       $('.cookieBubble').show();
-			    }
-			}
-
-			
-			$('.accept-btn').click(function(event) {
-				setcookie();
-			});
-
-			function setcookie() { 
-			    setCookie("cookieBubble", 30); 
-			}*/
-
 
 
     return this.each(function() { cookieApp.checkCookie(); });
